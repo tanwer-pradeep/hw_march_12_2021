e630c06400145ee89ac6e99860f800e1eab958ef
tanwer-pradeep <pradeepsinghonline@gmail.com>
2021-03-14 23:12:50 +0530

    changed help.js

--- help.js
+++ help.js
@@ -1,11 +1,14 @@
 function help(){
     console.log(`
         List of all commands :-
-        1. node wcat.js filepath
-        2. node wcat.js -s filepath
-        3. node wcat.js -b filepath
-        4. node wcat.js -n filepath
-        5. node wcat.js -help
+        1. node wcat.js filepath --> (to read the data from file)
+        2. node wcat.js -s filepath --> (to change all multiple line breaks to single line break)
+        3. node wcat.js -b filepath --> (to numbered the non empty lines)
+        4. node wcat.js -n filepath --> (to numbered all lines in the file)
+        5. node wcat.js filepath1 > filepath2 --> (to overwrite the data of filepath2 
+                                                    by the data of filepath1)
+        6. node wcat.js filepath1 >> filepath2 --> (to append all the data of filepath1 to filepath2)
+        7. node wcat.js -help --> (for help)
     `);
 }
 
